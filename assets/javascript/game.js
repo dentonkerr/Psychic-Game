8e7aad4b26c53af2aafa9c1ecc3c9c2e5c967046
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2017-12-08 14:40:03 -0500

    pseudo coding js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//Wins and losses start at 0
+
+//Guesses start at 9
+
+//Generate random letter from alphabet to guess
+
+//Game starts when letter is pressed. Continues until either letter is selected, or 9 guesses are gone. 
+
+//Display letters guessed and remove them from selection
+
+//If random letter is guessed, wins +1
+
+//If random letter is not guessed (guesses = 0), losses +1
+
+//If either guesses left = 0, or letter is guessed, game restarts on next letter press. Chooses new random letter, guesses so far resets, guesses left goes to 9
+
