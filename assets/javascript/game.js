2fdd540f351eae8a4b84c9082bbd2cf8564dfbfe
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2017-12-08 20:48:30 -0500

    more pseudo coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,12 +13,31 @@ document.getElementById("lossStart").innerHTML = lossesStart;
 //Guesses start at 9
 document.getElementById("guessStart").innerHTML = guesses;
 
+//alreadyGuessed array display
+document.getElementById("alreadyGuessed").innerHTML = alreadyGuessed;
+
 //Generate random letter from alphabet to guess
 randomLetter = alphabet[Math.floor(Math.random()*alphabet.length)];
 
 console.log(randomLetter);
 
 
+//document.onkeypress = function() {game()};
+//function game () {
+//    if (guessedLetter === randomLetter);
+//        winsStart++;
+//            function resetGame() {
+//                winsStart = 0;
+//                lossesStart = 0;
+//                guesses = 9;
+//                alreadyGuessed = [];
+//            }
+//    else (guessedLetter !== randomLetter);
+//        guesses--;
+//        alreadyGuessed.push(guessedLetter);
+//            if (guesses < 1); 
+//                resetGame();
+//}
 
 //when a key is pressed;
     //if - key matches randomLetter
@@ -26,5 +45,5 @@ console.log(randomLetter);
         //reset game (guesses back to 9, alreadyGuessed cleared, new random letter)
     //else - key does not match randomLetter
         //decrease guesses -1
-        //push letter to alreadyGuessed array
+        //push letter to alreadyGuessed array and make it not selectable again this game
         //continue these 2 steps until guesses = 0 = reset game (lossesStart +1, guesses back to 9, alreadyGuessed cleared, new random letter)
